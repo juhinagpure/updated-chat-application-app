9cba227c2eba278b5a3d450acd615e09b2fef219
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-04 18:30:22 +0530

    passes different props like height,projectId,username and usersecret in chatEngine

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import './App.css';
 const App = () => {
     return (
        <ChatEngine
-       
+       height="100vh"
+       projectID="a29ef303-622d-4e0e-954f-79cc81be8de7"
+       userName="Juhi123"
+       userSecret="123123"
        
        />
     )
 }
+
+export default App
