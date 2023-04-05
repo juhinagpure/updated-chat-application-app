23560d10ec9fd88d2d441d5feea6af3f0ab3ad94
JUHI NAGPURE <juhinagpure143@gmail.com>
2023-04-05 16:28:18 +0530

    added same content of MyMessage to TheirMessage

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,19 @@
-const MyMessage = () => {
+const MyMessage = ({Message}) => {
+    if(Message?.attachmnts?.length>0) {
+    return(
+        <img
+        src={Message.attachmnts[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{float:'right'}}
+        />
+    )
+}
     return (
-     <div>
-         MyMessage
+     <div className="message" style={{float:'right',marginRight:'18px',color:'white',backgroundColor:'#3B2A50'}}>
+         {Message.text}
      </div>
-    ) 
+    );
  }
  
  export default MyMessage;
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -8,10 +8,22 @@ const TheirMessage = ({lastMessage,message}) => {
                 className="message-avatar"
                 style={{backgroundImage:'url(${message?.sender?.avatar})'}}
                 />
-            )
-         }
+            )}
+{Message?.attachmnts?.length>0
+    ?(
+        <img
+        src={Message.attachmnts[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{float:'right'}}
+        />
+    ) : (
+        <div className="message" style={{float:'right',marginRight:'18px',color:'white',backgroundColor:'#3B2A50'}}>
+        {Message.text}
+    </div> 
+    )
+    }   
      </div>
-    ) 
- }
- 
+    );
+}
  export default TheirMessage;
